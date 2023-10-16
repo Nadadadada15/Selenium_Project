70aa4e37a92b8f96acdddfd5d8c338e39677a093
Nada Nikolic <ddanikoo15@gmail.com>
2023-10-17 00:00:13 +0200

    Verify Input types on SignUpPage

--- src/test/java/Tests/SignUpTests.java
+++ src/test/java/Tests/SignUpTests.java
@@ -1,13 +1,15 @@
 package Tests;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
 import org.testng.annotations.Test;
 
 public class SignUpTests extends BasicTest {
 
 
-    @Test
-    public void verifySignUpPageUrl(){
+    @Test(priority = 1, retryAnalyzer = IRetryAnalyzer.class)
+    public void verifySignUpPageUrl() {
 
         navPage.clickOnSignUpBtn();
         wait
@@ -16,5 +18,21 @@ public class SignUpTests extends BasicTest {
 
 
     }
+
+    @Test (priority = 2,retryAnalyzer = IRetryAnalyzer.class)
+    public void checkInputTypes() {
+        navPage.clickOnSignUpBtn();
+        wait
+                .withMessage("User Should Be on SignUp page")
+                .until(ExpectedConditions.urlContains("/signup"));
+
+        Assert.assertEquals(signUpPage.getNameInputField().getAttribute("type"), "text");
+        Assert.assertEquals(signUpPage.getEmailInputField().getAttribute("type"), "email");
+        Assert.assertEquals(signUpPage.getPasswordInputField().getAttribute("type"), "password");
+        Assert.assertEquals(signUpPage.getConfPasswordInputField().getAttribute("type"), "password");
+
+    }
+
+
 
 }
