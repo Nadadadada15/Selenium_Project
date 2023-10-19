85342dc9eae6470503e289d38ee643dd9ae9e259
Nada Nikolic <ddanikoo15@gmail.com>
2023-10-19 11:20:41 +0200

    Verify EN language change funtionality

--- src/test/java/Tests/LocaleTests.java
+++ src/test/java/Tests/LocaleTests.java
@@ -6,14 +6,19 @@ import org.testng.annotations.Test;
 public class LocaleTests extends BasicTest{
 
     @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
-    public void verifyESLanguageChange(){
+    public void verifyESLanguageChange() {
         navPage.clickOnLangBtn();
         navPage.clickOnEspLangLink();
         navPage.waitUntiltextIsInEsp();
-
-
     }
 
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void verifyENLanguageChange()  {
+        navPage.clickOnLangBtn();
+        navPage.clickOnEngLangLink();
+        navPage.waitUntiltextIsInEng();
+    }
 
 
 
