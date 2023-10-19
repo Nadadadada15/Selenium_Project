dccfd329a8c437b836a2a624ea4247220993302c
Nada Nikolic <ddanikoo15@gmail.com>
2023-10-19 10:45:01 +0200

    Verify url paths for non authenticated users

--- src/test/java/Tests/AuthRoutesTests.java
+++ src/test/java/Tests/AuthRoutesTests.java
@@ -1,4 +1,46 @@
 package Tests;
 
-public class AuthRoutesTests {
+import Retry.RetryAnalyzer;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest{
+    String baseUrl="https://vue-demo.daniel-avellaneda.com";
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated(){
+
+        driver.navigate().to(baseUrl + "/home");
+      Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login","Url should display '/login' path");
+
+    }
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToProfileUrlIfNotAuthenticated(){
+
+        driver.navigate().to(baseUrl + "/profile");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url should display '/login' path");
+
+
+    }
+
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToAdminCitiesUrlIfNotAuthenticated(){
+
+        driver.navigate().to(baseUrl + "/admin/cities");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url should display '/login' path");
+
+
+    }
+
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToAdminUsersUrlIfNotAuthenticated(){
+
+        driver.navigate().to(baseUrl + "/admin/users");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url should display '/login' path");
+
+
+    }
+
+
 }
