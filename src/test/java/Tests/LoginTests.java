b1be48066e1a6ed98513be60c54b442bc980432a
Nada Nikolic <ddanikoo15@gmail.com>
2023-10-19 10:42:20 +0200

    Add missing line in LogIn tests

--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -95,6 +95,8 @@ public class LoginTests extends BasicTest {
         logInPage.getEmailField().sendKeys(email);
         logInPage.getPasswordField().sendKeys(password);
 
+        logInPage.clickOnLoginBtn();
+
         wait
                 .withMessage("Url should contain '/home' path ")
                 .until(ExpectedConditions.urlContains("/home"));
