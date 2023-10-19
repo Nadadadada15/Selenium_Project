487862ede8a89df4fd5c7dfcdd99677980b59926
Nada Nikolic <ddanikoo15@gmail.com>
2023-10-19 11:15:21 +0200

    Verify ES language change funtionality

--- src/test/java/Tests/LocaleTests.java
+++ src/test/java/Tests/LocaleTests.java
@@ -1,4 +1,25 @@
 package Tests;
 
-public class LocaleTests {
+import Retry.RetryAnalyzer;
+import org.testng.annotations.Test;
+
+public class LocaleTests extends BasicTest{
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void verifyESLanguageChange(){
+        navPage.clickOnLangBtn();
+        navPage.clickOnEspLangLink();
+        navPage.waitUntiltextIsInEsp();
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
